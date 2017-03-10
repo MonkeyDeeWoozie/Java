5ec30fe212ee44e6a508d4af1813845476b29c96
Mickael MARCELAT <Mickael.MARCELAT@Digital-Grenoble.local>
2017-03-10 17:22:40 +0100

    create java repository

--- Reseau_social/src/Main.java
+++ Reseau_social/src/Main.java
@@ -16,26 +16,31 @@ public class Main
         System.out.println(ville);
         */
 
-        char boucle = "o";
+        String boucle = "oui";
 
-        
+        while(boucle.equals("oui")) {
 
-        Scanner entrer = new Scanner(System.in);
+            Scanner entrer = new Scanner(System.in);
 
-        System.out.println("Ton prénom :");
-        String res1 = entrer.nextLine();
+            System.out.println("Ton prénom :");
+            String res1 = entrer.nextLine();
 
-        System.out.println("Ton nom :");
-        String res2 = entrer.nextLine();
+            System.out.println("Ton nom :");
+            String res2 = entrer.nextLine();
 
-        System.out.println("Ton age :");
-        int res3 = entrer.nextInt();
+            System.out.println("Ton age :");
+            int res3 = entrer.nextInt();
 
-        entrer.nextLine();
+            entrer.nextLine();
 
-        System.out.println("Ta ville :");
-        String res4 = entrer.nextLine();
+            System.out.println("Ta ville :");
+            String res4 = entrer.nextLine();
 
-        System.out.println("Bonjour, " + res1 + " " + res2 + " tu as " + res3 + " ans et tu habites à " + res4 + " !");
+            System.out.println("Bonjour, " + res1 + " " + res2 + " tu as " + res3 + " ans et tu habites à " + res4 + " !");
+
+            System.out.println("Encore une fois ? [oui/non]");
+            boucle = entrer.nextLine();
+
+        }
     }
 }
