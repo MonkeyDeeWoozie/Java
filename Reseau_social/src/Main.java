d75041aeecdce4270ebfea3a6cf98b7bd517f1e5
Mickael MARCELAT <Mickael.MARCELAT@Digital-Grenoble.local>
2017-03-10 17:05:16 +0100

    create java repository

---
+++ Reseau_social/src/Main.java
@@ -0,0 +1,41 @@
+import java.util.*;
+
+public class Main
+{
+    public static void main(String[] args)
+    {
+        /*
+        String prenom = "Mickael";
+        String nom = "Marcelat";
+        int age = 27;
+        String ville = "Grenoble";
+
+        System.out.println(prenom);
+        System.out.println(nom);
+        System.out.println(age);
+        System.out.println(ville);
+        */
+
+        char boucle = "o";
+
+        
+
+        Scanner entrer = new Scanner(System.in);
+
+        System.out.println("Ton prénom :");
+        String res1 = entrer.nextLine();
+
+        System.out.println("Ton nom :");
+        String res2 = entrer.nextLine();
+
+        System.out.println("Ton age :");
+        int res3 = entrer.nextInt();
+
+        entrer.nextLine();
+
+        System.out.println("Ta ville :");
+        String res4 = entrer.nextLine();
+
+        System.out.println("Bonjour, " + res1 + " " + res2 + " tu as " + res3 + " ans et tu habites à " + res4 + " !");
+    }
+}
